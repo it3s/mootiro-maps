9200b8d67ead2633d518506b1b3ef26dcad2b130
André Casimiro <ah.casimiro@gmail.com>
2012-02-01 18:23:11 -0200

    Adds more fields to Community
    OBS: there is a address field in Community with a example for django-easy-maps app.

--- community/models.py
+++ community/models.py
@@ -4,4 +4,11 @@ class Community(models.Model):
 
     name = models.CharField(max_length=256)
     # Auto-generated url slug. It's not editable via ModelForm.
-    slug = models.CharField(max_length=256, editable=False)
+    slug = models.SlugField(max_length=256)
+
+    population = models.IntegerField()  # number of inhabitants
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    address = models.CharField(max_length=1024)
--- community/views.py
+++ community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from mootiro_komoo.community.models import Community
 from mootiro_komoo.community.forms import CommunityForm
 
 def new (request):
@@ -15,3 +16,8 @@ def save (request):
     community = CommunityForm(request.POST)
     community.save()
     return render_to_response('new.html')
+
+def map (request, slug):
+    community = Community.objects.get(slug=slug)
+    return render_to_response('map.html', {'community': community},
+            context_instance=RequestContext(request))
