a802232e4f95062ee9abedb4aa88109722335eb8
Nando Florestan <nandoflorestan@gmail.com>
2012-02-09 12:15:40 -0200

    Use django_annoying in proposal views.

--- mootiro_komoo/komoo/proposal/forms.py
+++ mootiro_komoo/komoo/proposal/forms.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals  # unicode by default
 from django.forms import ModelForm, CharField
+from annoying.decorators import autostrip
 from tinymce.widgets import TinyMCE
 from .models import Proposal
 
 
+@autostrip
 class ProposalForm(ModelForm):
     '''https://github.com/aljosa/django-tinymce/blob/master/docs/usage.rst'''
     # content = CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
--- mootiro_komoo/komoo/proposal/views.py
+++ mootiro_komoo/komoo/proposal/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals  # unicode by default
 from django.shortcuts import render_to_response, redirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from annoying.decorators import render_to
 from ..need.models import Need
 from .models import Proposal
 from .forms import ProposalForm
@@ -14,13 +15,13 @@ def view(request, need_slug, id):
     return render_to_response('proposal_view.html', dict(proposal=proposal))
 
 
+@render_to('proposal_edit.html')
 def new(request, need_slug):
-    return render_to_response('proposal_edit.html',
-            dict(form=ProposalForm(),
-                 action=reverse('save_proposal', args=(need_slug,))),
-            context_instance=RequestContext(request))
+    return dict(form=ProposalForm(),
+                action=reverse('save_proposal', args=(need_slug,)))
 
 
+@render_to('proposal_edit.html')
 def save(request, need_slug):
     need = Need.objects.get(slug=need_slug)
     proposal = Proposal(need=need)
@@ -29,13 +30,11 @@ def save(request, need_slug):
         proposal = form.save()
         return redirect(view, need_slug, proposal.id)
     else:
-        return render_to_response('proposal_edit.html', dict(form=form),
-            context_instance=RequestContext(request))
+        return dict(form=form)
 
 
+@render_to('proposal_edit.html')
 def edit(request, need_slug, id):
     p = Proposal.objects.get(id=id)
-    context = dict(form=ProposalForm(instance=p),
-                   action=reverse('save_proposal', args=(need_slug,)))
-    return render_to_response('proposal_edit.html', context,
-            context_instance=RequestContext(request))
+    return dict(form=ProposalForm(instance=p),
+                action=reverse('save_proposal', args=(need_slug,)))
