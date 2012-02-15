af3566238620c371b49146f726f79f428f00ad84
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-02-15 11:01:51 -0200

    removing old folders

--- mootiro_komoo/komoo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from need.models import NeedCategory
-
-class NeedCategoryAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(NeedCategory, NeedCategoryAdmin)
