2d55b7ab0ac3e46af404ffea081abc0ab6db593e
André Casimiro <ah.casimiro@gmail.com>
2012-02-17 12:54:33 -0200

    Communities are persisted in geolocation

--- mootiro_komoo/apps/community/forms.py
+++ mootiro_komoo/apps/community/forms.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals  # unicode by default
-from django.forms import ModelForm, Form, CharField
+
+from django import forms
+
 from komoo_map.widgets import AddressWithMapWidget
 from community.models import Community
 
 
-class CommunityForm(ModelForm):
+class CommunityForm(forms.ModelForm):
     class Meta:
         model = Community
+    
+    geometry = forms.CharField(widget=forms.HiddenInput())
 
 
-class CommunityMapForm(Form):
-    map = CharField(widget=AddressWithMapWidget, required=False)
+class CommunityMapForm(forms.Form):
+    map = forms.CharField(widget=AddressWithMapWidget, required=False)
