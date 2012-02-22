578113bd1b8d79b067c93503e36d257f64def56e
André Casimiro <ah.casimiro@gmail.com>
2012-02-22 15:05:33 -0200

    NeedForm has never been this crispy!

--- mootiro_komoo/apps/need/forms.py
+++ mootiro_komoo/apps/need/forms.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals  # unicode by default
 
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Reset
 
 from main.fields import TagsField
 from main.widgets import JQueryAutoComplete, Tagsinput, ImageSwitchMultiple
@@ -19,18 +21,19 @@ class NeedForm(forms.ModelForm):
     # with reverse_lazy function, which is not implemented in Django 1.3 yet.
     community_name = forms.CharField(widget=JQueryAutoComplete(
         "/community/search_by_name", value_field='community'))
-    
+
     categories = forms.ModelMultipleChoiceField(
-                    queryset=NeedCategory.objects.all(),
-                    widget=ImageSwitchMultiple)
+        queryset=NeedCategory.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
 
     tags = TagsField(widget=Tagsinput(autocomplete_url="/need/tag_search"))
 
     def __init__(self, *a, **kw):
+        self.helper = MooHelper()
         super(NeedForm, self).__init__(*a, **kw)
         if 'instance' in kw and kw['instance']:  # self.is_bound does not work properly
             self.fields.pop('community_name')
-
 
     def clean_community(self):
         return Community.objects.get(id=self.cleaned_data['community'])
