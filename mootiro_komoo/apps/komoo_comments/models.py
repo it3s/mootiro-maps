337faefc28b0c8e9e8aad6c0c7c64a1dc18cb02e
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-02-23 15:06:22 -0200

    generic relations on comments + bug fix (infinite recursion on  adding root comments)

--- mootiro_komoo/apps/komoo_comments/models.py
+++ mootiro_komoo/apps/komoo_comments/models.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals  # unicode by default
 from django.db import models
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
+    """
+    Comments models. Should dinamically reference any table/object.
+    For querying purposes we need to add the above entry on those models that need to have Comments:
+        comments = generic.GenericRelation(Comment)
+    """
     author = models.ForeignKey(User, blank=True, null=True)
     comment = models.CharField(max_length=1024)
     parent = models.ForeignKey('Comment', null=True, blank=True, related_name="comment_parent")
     sub_comments = models.IntegerField(blank=True, default=0)
     pub_date = models.DateTimeField(auto_now_add=True)
+    # dynamic ref
+    content_type = models.ForeignKey(ContentType,  null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     def save(self, *args, **kwargs):
         if self.parent:
