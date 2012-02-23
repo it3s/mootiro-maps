29acc0fa9f754abf803517667a5356fe4afe3fc1
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-02-23 17:34:59 -0200

    adding staticfiles on comments templatetags

--- mootiro_komoo/apps/komoo_comments/templatetags/comment_tags.py
+++ mootiro_komoo/apps/komoo_comments/templatetags/comment_tags.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.inclusion_tag('comments/comments_templatetag.html')
 def comments(comments):
     return {'form_comment': FormComment(), 'comments_list': comments}
+
+
+@register.inclusion_tag('comments/comments_staticfiles.html')
+def comments_staticfiles():
+    return {}
