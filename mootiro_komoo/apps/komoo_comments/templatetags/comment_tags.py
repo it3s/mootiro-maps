88ec0921b9b6d8c228f88e4dbd3104bd54b07bdf
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-02-24 15:52:02 -0200

    saving subcomments with proper contenttypes references

--- mootiro_komoo/apps/komoo_comments/templatetags/comment_tags.py
+++ mootiro_komoo/apps/komoo_comments/templatetags/comment_tags.py
@@ -35,7 +35,7 @@ def comments(context, content_object, arg1='', arg2=''):
     c = ContentType.objects.get_for_model(content_object)
     form = FormComment(initial={'content_type_id': c.id, 'object_id': content_object.id})
 
-    comments = comments_list(content_object=content_object, width=width, height=height, root=True).content
+    comments = comments_list(content_object=content_object, context=context,  width=width, height=height, root=True).content
     return {'form_comment': form, 'comments_list': comments}
 
 
