a8de9500634be8e0d76e6a73f63a0bcdeae80226
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-03-19 17:04:10 -0300

    hasystack search indexing for community

--- mootiro_komoo/apps/community/search_indexes.py
+++ mootiro_komoo/apps/community/search_indexes.py
@@ -2,18 +2,18 @@
 from datetime import datetime
 from haystack import indexes
 from haystack import site
-from komoo_resource.models import Resource
+from community.models import Community
 
 
-class ResourceIndex(indexes.SearchIndex):
+class CommunityIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.EdgeNgramField(model_attr='name', boost=2.0)
+    slug = indexes.EdgeNgramField(model_attr='slug', boost=2.0)
     creator = indexes.CharField(model_attr='creator')
     creation_date = indexes.DateTimeField(model_attr='creation_date')
-    description = indexes.CharField(model_attr='description')
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return Resource.objects.filter(creation_date__lte=datetime.now())
+        return Community.objects.filter(creation_date__lte=datetime.now())
 
-site.register(Resource, ResourceIndex)
+site.register(Community, CommunityIndex)
