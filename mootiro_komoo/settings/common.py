5d3fdb9294c20ddda47b8e727a0b8837d0c73594
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-03-19 11:58:51 -0300

    translations

--- mootiro_komoo/settings/common.py
+++ mootiro_komoo/settings/common.py
@@ -31,6 +31,10 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT, 'locale'),
+)
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
