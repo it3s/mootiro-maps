0564d607245fe624609654d93fcdbb08f413ab12
André Casimiro <ah.casimiro@gmail.com>
2012-03-21 17:08:45 -0300

    Proto implementation of filter form submission

--- mootiro_komoo/apps/komoo_map/static/js/map_filter.js
+++ mootiro_komoo/apps/komoo_map/static/js/map_filter.js
@@ -1,1 +1,27 @@
-console.log("OIEE");
+
+// Add center functionality
+$("#filter-center").click(function () {
+    alert("Não clique aí!");
+});
+
+
+$("#filter-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var radius = $("#filter-radius");
+    var center = "-23.56517,-46.773133";
+    var url = dutils.urls.resolve('radial_search');
+    
+    $.ajax({
+        type: 'GET',
+        url: dutils.urls.resolve('radial_search'),
+        data: {
+            radius: radius,
+            center: "-23.56517,-46.773133"
+        }
+    }).success(function (data) {
+        console.log(data);
+    });
+    
+    return false;
+});
