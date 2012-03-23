c88e6d59aaf1b2dc53e6ac5cd4266933bb8dced9
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-03-23 16:30:09 -0300

    fixing js_utils problem with ajax_select

--- mootiro_komoo/lib/django_js_utils/management/commands/js_urls.py
+++ mootiro_komoo/lib/django_js_utils/management/commands/js_urls.py
@@ -12,7 +12,7 @@ from django_js_utils import settings as app_settings
 
 RE_KWARG = re.compile(r"(\(\?P\<(.*?)\>.*?\))") #Pattern for recongnizing named parameters in urls
 RE_ARG = re.compile(r"(\(.*?\))") #Pattern for recognizing unnamed url parameters
-    
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -27,7 +27,7 @@ class Command(BaseCommand):
         urls_file.write("dutils.conf.urls = ")
         simplejson.dump(js_patterns, urls_file)
         print "Done generating Javascript urls file %s" % app_settings.URLS_JS_GENERATED_FILE
-    
+
     @staticmethod
     def handle_url_module(js_patterns, module_name, prefix=""):
         """
