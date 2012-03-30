8a2a3a86d505c78e94dbcce6a68caf79cb350a65
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-03-30 15:44:29 -0300

    initial results display

--- mootiro_komoo/apps/main/static/js/komoo_search.js
+++ mootiro_komoo/apps/main/static/js/komoo_search.js
@@ -1,7 +1,8 @@
 // alert('search js');
 (function($){
     var form_search = $('#search'),
-        search_field = $('#search-bar');
+        search_field = $('#search-bar'),
+        search_results = $('#search-results');
         csrftoken= getCookie('csrftoken');
 
     form_search.submit(function(evt){
@@ -15,6 +16,19 @@
             dataType: 'json',
             success: function(data){
                 console.dir(data);
+                search_results.find('ul').remove();
+
+                lis = '';
+                data.result.forEach(function(obj){
+                    lis += '<li>' + obj.name +  '</li>';
+                });
+
+                search_results.append('' +
+                    '<ul>' +
+                        lis +
+                    '</ul>'
+                );
+
             }
         });
     });
