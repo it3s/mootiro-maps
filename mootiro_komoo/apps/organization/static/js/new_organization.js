e939208f9a99efa57ff278f911548ca8f4c7749d
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-04-17 14:04:20 -0300

    fixed bug when pressing enter on org form instead of tabs

--- mootiro_komoo/apps/organization/static/js/new_organization.js
+++ mootiro_komoo/apps/organization/static/js/new_organization.js
@@ -9,45 +9,59 @@ var forms;
 
 /* checks if we are acessing the map from the button, or the map */
 var btn_mode = Boolean(getUrlVars().frommap);
-
 if ( ! btn_mode ){
-$organization_fields.hide();
-$branch_fields.hide();
-$form_organization_btns.hide();
+    $organization_fields.hide();
+    $branch_fields.hide();
+    $form_organization_btns.hide();
 
-
-$id_org_name_autocomplete.autocomplete({
-    source: "/organization/search_by_name/",
-    focus: function(event, ui) {
-        $id_org_name_autocomplete.val(ui.item.label);
-        return false;
-    },
-    select: function(event, ui) {
-        $id_org_name_autocomplete.val(ui.item.label);
-        $id_org_name.val(ui.item.value);
-
-        // org ja exite -> somente branch
+    var formsNewOrg = function(){
+        $organization_fields.fadeIn('slow');
         $branch_fields.fadeIn('slow');
-        $organization_fields.hide();
         $form_organization_btns.fadeIn('slow');
-        forms = ['branch'];
+        forms = ['organization', 'branch'];
+    };
+
+
+    $id_org_name_autocomplete.autocomplete({
+        source: "/organization/search_by_name/",
+        focus: function(event, ui) {
+            $id_org_name_autocomplete.val(ui.item.label);
+            return false;
+        },
+        select: function(event, ui) {
+            $id_org_name_autocomplete.val(ui.item.label);
+            $id_org_name.val(ui.item.value);
+
+            // org ja exite -> somente branch
+            $branch_fields.fadeIn('slow');
+            $organization_fields.hide();
+            $form_organization_btns.fadeIn('slow');
+            forms = ['branch'];
+
+            return false;
+        },
+        change: function(event, ui) {
+            if(!ui.item || ! $id_org_name_autocomplete.val()){
+                // Nova org ->  (branch + org)
+                formsNewOrg();
+            }
+        }
+     });
+
+    $submit_btn.click(function(evt){
+        $id_form_control.val(forms.join('|'));
+    });
+
+    shortcut.add('enter', function(evt){
+        evt.preventDefault();
+        if (! $id_org_name.val()){
+            formsNewOrg();
+        }
 
         return false;
-    },
-    change: function(event, ui) {
-        if(!ui.item || ! $id_org_name_autocomplete.val()){
-            // Nova org ->  (branch + org)
-            $organization_fields.fadeIn('slow');
-            $branch_fields.fadeIn('slow');
-            $form_organization_btns.fadeIn('slow');
-            forms = ['organization', 'branch'];
-        }
-    }
- });
+    }, {target: 'id_org_name_autocomplete'});
 
-$submit_btn.click(function(evt){
-    $id_form_control.val(forms.join('|'));
-});
+
 
 } else {
 
