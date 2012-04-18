414ad0a75427c707074cfc90e799e4e6dc63de43
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-04-18 16:40:10 -0300

    image picker templatetag

--- mootiro_komoo/lib/fileupload/templatetags/upload_tags.py
+++ mootiro_komoo/lib/fileupload/templatetags/upload_tags.py
@@ -17,3 +17,53 @@ def load_files(context, obj=None):
         object_id = ''
         content_type = ''
     return dict(files=files, object_id=object_id, content_type=content_type)
+
+
+@register.simple_tag
+def image_picker(field="logo"):
+    return """
+    <script type="text/javascript">
+        $(function(){
+            $('#files-list').prepend("" +
+                "<div id='logo-advise'>Escolha uma imagem abaixo para ser o logo</div>"
+            );
+
+            if ($('#id_%(field)s').val()) {
+                $('.file-entry[file-id=' + $('#id_%(field)s').val() + ']').addClass('file-entry-main');
+            }
+
+            $('.file-entry').live('click', function(){
+                $('.file-entry-main').removeClass('file-entry-main');
+                $(this).addClass('file-entry-main');
+            });
+
+            $('form').submit(function(evt){
+                var id = $('.file-entry-main').attr('file-id');
+                $('#id_%(field)s').val(id);
+            })
+        });
+
+    </script>
+    <style type="text/css">
+        .file-entry:hover{
+            background-color: rgb(64, 146, 169);
+            color: #cccccc;
+        }
+        .file-entry:hover span{
+            color: #cccccc;
+        }
+        .file-entry-main{
+            background-color: rgb(64, 146, 169);
+            color: #cccccc;
+        }
+        .file-entry-main span{
+            color: #cccccc;
+        }
+        #logo-advise{
+            text-align: center;
+            color: #999999;
+            padding: 10px;
+            font-weight: bold;
+        }
+    </style>
+    """ % {'field': field}
