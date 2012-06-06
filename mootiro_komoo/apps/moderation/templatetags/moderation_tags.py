aad0c60c76340cddc32f9f8dc915180207c3ea75
Luiz Armesto <luiz.armesto@gmail.com>
2012-06-06 16:09:49 -0300

    some fixes

--- mootiro_komoo/apps/moderation/templatetags/moderation_tags.py
+++ mootiro_komoo/apps/moderation/templatetags/moderation_tags.py
@@ -12,7 +12,7 @@ def report_content(context, obj, button_type='button'):
     """The syntax: {% report_content object %}"""
     user = context.get('user', None)
     reports = get_reports_by_user(user, obj)
-    disabled = reports.count() > 0
+    disabled = True if reports else False
     return dict(app_label=obj._meta.app_label,
                 model_name=obj._meta.module_name,
                 id=obj.id,
@@ -27,7 +27,7 @@ def delete_button(context, obj):
     user = context.get('user', None)
     deletion_requests = get_reports_by_user(user, obj,
                                             reason=Report.DELETION_REQUEST)
-    disabled = not can_delete(obj, user) and deletion_requests.count() > 0
+    disabled = True if not can_delete(obj, user) and deletion_requests else False
     return dict(app_label=obj._meta.app_label,
                 model_name=obj._meta.module_name,
                 id=obj.id,
