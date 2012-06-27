fb0033a72fd6dbbb278e15bd0db4542133e05d0d
André Casimiro <ah.casimiro@gmail.com>
2012-06-27 12:45:48 -0300

    Adds pagination to frontpage

--- mootiro_komoo/apps/update/views.py
+++ mootiro_komoo/apps/update/views.py
@@ -8,6 +8,7 @@ import itertools
 from annoying.decorators import render_to
 from reversion.models import Version, VERSION_DELETE
 
+from main.utils import paginated_query, sorted_query, filtered_query
 from .models import Update, News
 
 
@@ -18,8 +19,13 @@ logger = logging.getLogger(__name__)
 def frontpage(request):
     logger.debug('acessing update > frontpage')
 
-    updates_number = 100
-    updates = Update.objects.order_by("-date")[0:100]
-    news = News.objects.order_by("-date")[0:100]
+    query_set = Update.objects
+    query_set = filtered_query(query_set, request)
+    sort_order = ['-date', 'comments_count']
+    updates_list = sorted_query(query_set, sort_order, request, default_order='-date')
+    updates_count = updates_list.count()
+    updates = paginated_query(updates_list, request, size=3)
 
-    return {'updates': updates, 'news': news}
+    news = News.objects.order_by("-date")
+
+    return dict(updates=updates, news=news)
