4cdadbb4db7c1aef9cb32735b89d74d6b93b21e1
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-07-06 16:51:08 -0300

    search popover box working with the backend

--- mootiro_komoo/apps/main/static/js/komoo_search.js
+++ mootiro_komoo/apps/main/static/js/komoo_search.js
@@ -20,17 +20,79 @@ $(function() {
     var showPopover = function(){
         $('#search-results-box').popover('show');
         $('.popover').css('top', parseInt($('.popover').css('top')) - 10);
+        $('.popover').css('left', parseInt($('.popover').css('left')) - 75);
     };
 
-    var showResults = function (results){
-        console.dir(results);
-        $('#search-results-box').popover({
-            placement: 'bottom',
-            selector: search_field,
-            trigger: 'manual',
-            title: 'teste<span id="search-box-close" >x</span>',
-            content: 'working?'
-        })
+    var showResults = function (result){
+
+        var results_list = '';
+        var results_count = 0;
+        var has_results = false;
+        var result_order = ['community', 'organization', 'need', 'resource'];
+        $.each(result_order, function(idx, key){
+            var val = result[key];
+            if (val.length && key !== 'google'){
+                results_list += '<li><div class="search-header">' +
+                    '<img src="/static/img/' + key + '.png" >' +
+                    '<div class="search-type-header" >' +
+                        titles[key] +
+                        '<span class="search-results-count"> ' +
+                            interpolate(
+                                ngettext("%s result", "%s results", val.length),
+                                [val.length]
+                            ) +
+                        '</span>' +
+                    '</div>' +
+               ' </div><ul class="search-result-entries">';
+                val.forEach(function(obj){
+                    results_list += '<li><a href="' +  obj.link + '" >' + obj.name +  '</a></li>';
+                    results_count++;
+                });
+                results_list += '</ul></li>';
+                has_results |= true;
+            } else {
+                // results_list = '<div class="search-no-results">' + gettext('No results found!') + '</div>';
+                has_results |= false;
+            }
+        });
+
+        // Google Results
+        var google_results = JSON.parse(result.google).predictions;
+        if (google_results && google_results.length){
+            var key = 'google';
+            results_list += '<li><div class="search-header">' +
+                '<img src="/static/img/' + key + '.png" >' +
+                '<div class="search-type-header" >' +
+                    titles[key] +
+                    '<span class="search-results-count"> ' +
+                        interpolate(
+                            ngettext("%s result", "%s results", google_results.length),
+                            [google_results.length]
+                        ) +
+                    '</span>' +
+                '</div>' +
+           ' </div><ul class="search-result-entries">';
+            google_results.forEach(function(obj){
+                results_list += '<li><a href="#" onclick="editor.goTo(\'' + obj.description + '\'); return false;" >' + obj.description +  '</a></li>';
+                results_count++;
+            });
+            results_list += '</ul></li>';
+            has_results |= true;
+        } else {
+            has_results |= false;
+        }
+
+
+        if (! has_results) {
+            results_list = '<div class="search-no-results">' + gettext('No results found!') + '</div>';
+        }
+
+        // Display results
+        $('#search-results-box').data('popover').options.title = '' +
+            results_count + ' Results <span id="search-box-close" >x</span>';
+
+        $('#search-results-box').data('popover').options.content = results_list;
+
         showPopover();
     };
 
@@ -47,6 +109,13 @@ $(function() {
                 showResults(data.result);
             }
         });
+
+    });
+
+    $('#search-results-box').popover({
+        placement: 'bottom',
+        selector: search_field,
+        trigger: 'manual',
 
     });
 
