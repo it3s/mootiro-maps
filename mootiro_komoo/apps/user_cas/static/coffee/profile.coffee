b7a02945a7546c7f16ee85ca584b39f862feb662
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-07-24 17:46:42 -0300

    creating initial backbone view and models

--- mootiro_komoo/apps/user_cas/static/coffee/profile.coffee
+++ mootiro_komoo/apps/user_cas/static/coffee/profile.coffee
@@ -1,11 +1,23 @@
 $ = jQuery
 
-window.Contribution = Backbone.Model.extend {}
+window.Contribution = Backbone.Model.extend
+    bla: () ->
+        console.log 'blaa'
 
 window.Contributions = Backbone.Collection.extend
     model: Contribution
 
-window.ContributionView = Backbone.View.extend {}
+window.ContributionView = Backbone.View.extend
+    tagName: 'li'
+    className: 'contribution'
+    initialize: () ->
+        _.bindAll this, 'render'
+        @template = _.template $('#contribution-template').html()
+
+    render: () ->
+        renderedContent = @template @model.toJSON()
+        $(@el).html renderedContent
+        this
 
 window.ContributionsView = Backbone.View.extend {}
 
--- mootiro_komoo/apps/user_cas/static/js/profile.js
+++ mootiro_komoo/apps/user_cas/static/js/profile.js
@@ -3,13 +3,30 @@
 
   $ = jQuery;
 
-  window.Contribution = Backbone.Model.extend({});
+  window.Contribution = Backbone.Model.extend({
+    bla: function() {
+      return console.log('blaa');
+    }
+  });
 
   window.Contributions = Backbone.Collection.extend({
     model: Contribution
   });
 
-  window.ContributionView = Backbone.View.extend({});
+  window.ContributionView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'contribution',
+    initialize: function() {
+      _.bindAll(this, 'render');
+      return this.template = _.template($('#contribution-template').html());
+    },
+    render: function() {
+      var renderedContent;
+      renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    }
+  });
 
   window.ContributionsView = Backbone.View.extend({});
 
