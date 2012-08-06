d9ac8e64b6cce04a789dcb8d9ab212b4c44c4287
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2012-08-06 12:59:03 -0300

    starting form and other boilerplates for projects

--- mootiro_komoo/apps/komoo_project/views.py
+++ mootiro_komoo/apps/komoo_project/views.py
@@ -1,6 +1,10 @@
 #! coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render_to_response, RequestContext
+from lib.taggit.models import TaggedItem
 
+from .forms import FormProject
+from .models import Project
 
 def project_list(request):
     #  TODO implement-me
@@ -10,4 +14,23 @@ def project_list(request):
 def project_view(request, project_slug=''):
     # TODO implement-me
     return {}
+
+
+def project_new(request, project_slug=''):
+    form = FormProject()
+    return render_to_response(
+            'project/new.html',
+            dict(form_project=form),
+            context_instance=RequestContext(request))
+
+
+def tag_search(request):
+    logger.debug('acessing project > tag_search')
+    term = request.GET['term']
+    qset = TaggedItem.tags_for(Project).filter(name__istartswith=term)
+    # qset = TaggedItem.tags_for(project)
+    tags = [t.name for t in qset]
+    return HttpResponse(simplejson.dumps(tags),
+                mimetype="application/x-javascript")
+
 
