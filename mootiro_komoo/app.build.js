7078ea6f825d803ad29a0daca5ea58a39485b63b
Luiz Armesto <luiz.armesto@gmail.com>
2012-10-15 13:22:03 -0300

    improving javascript tests

--- mootiro_komoo/app.build.js
+++ mootiro_komoo/app.build.js
@@ -3,9 +3,12 @@
     baseUrl: '.build',
     dir: '.build/min',
     paths: {
-        jquery: "empty:",
-        underscore: "empty:",
-        backbone: "empty:"
+        'jquery': "empty:",
+        'underscore': "empty:",
+        'backbone': "empty:",
+        'async': '../apps/main/static/lib/requirejs/async',
+        'goog': '../apps/main/static/lib/requirejs/goog',
+        'propertyParser': '../apps/main/static/lib/requirejs/propertyParser'
     },
     modules: [
         {
