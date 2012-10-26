6724f36f80642660a65fcdcbc6e588f7235393c9
André Casimiro <ah.casimiro@gmail.com>
2012-10-26 15:29:57 -0200

    Page for easily get a refresh token for importsheet app

---
+++ mootiro_komoo/apps/importsheet/token.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+'''
+These views exists as a easy way to get a refresh code for a given google 
+account. Once you get the refresh code store it securely!
+'''
+from __future__ import unicode_literals
+import requests
+import simplejson
+
+from django.conf import settings
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
+from annoying.decorators import render_to
+from authentication.utils import encode_querystring
+
+
+def refresh_token(request):
+    '''Getting authorization from the user'''
+    redirect_uri = request.build_absolute_uri(reverse('importsheet_refresh_token_authorized'))
+    params = {
+        'client_id': settings.GOOGLE_APP_ID,
+        'redirect_uri': redirect_uri,
+        # below a space separated list of permissions
+        'scope': 'https://www.googleapis.com/auth/drive '
+                 'https://www.googleapis.com/auth/drive.file',
+        'access_type': 'offline',
+        'response_type': 'code',
+        'approval_prompt': 'force',
+    }
+    url = 'https://accounts.google.com/o/oauth2/auth'
+    url += '?' + encode_querystring(params)
+    return redirect(url)
+
+
+@render_to('importsheet/refresh_token.html')
+def refresh_token_authorized(request):
+    '''Exchange the authorization code for an refresh token and displays it.'''
+    error = request.GET.get('error', None)
+    if error:
+        error_description = request.GET.get('error_description', None)
+        return dict(error=error_description)
+
+    redirect_uri = request.build_absolute_uri(reverse('importsheet_refresh_token_authorized'))
+    params = {
+        'client_id': settings.GOOGLE_APP_ID,
+        'client_secret': settings.GOOGLE_APP_SECRET,
+        'code': request.GET.get('code'),
+        'redirect_uri': redirect_uri,
+        'grant_type': 'authorization_code',
+    }
+    url = 'https://accounts.google.com/o/oauth2/token'
+    resp = requests.post(url, data=params)
+    access_data = simplejson.loads(resp.text)
+    refresh_token = access_data['refresh_token']
+
+    return dict(refresh_token=refresh_token)
--- mootiro_komoo/apps/importsheet/urls.py
+++ mootiro_komoo/apps/importsheet/urls.py
@@ -7,3 +7,10 @@ from mootiro_komoo.urls import prepare_regex as pr
 urlpatterns = patterns('importsheet.views',
     url(pr(r'^poc/?$'), 'poc', name='importsheet'),
 )
+
+urlpatterns += patterns('importsheet.token',
+    url(pr(r'^refresh_token/?$'), 'refresh_token',
+            name='importsheet_refresh_token'),
+    url(pr(r'^refresh_token_authorized/?$'), 'refresh_token_authorized',
+            name='importsheet_refresh_token_authorized'),
+)
