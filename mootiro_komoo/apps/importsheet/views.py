67447088eab8aea35227552070adb6676f1a0ab0
André Casimiro <ah.casimiro@gmail.com>
2012-10-29 08:34:55 -0200

    Initial poc

--- mootiro_komoo/apps/importsheet/views.py
+++ mootiro_komoo/apps/importsheet/views.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+'''
+
+Reference:
+    https://developers.google.com/api-client-library/python/reference/pydoc
+    https://developers.google.com/apis-explorer/
+'''
 from __future__ import unicode_literals
+
+from apiclient.discovery import build
 
 from annoying.decorators import render_to
 
@@ -7,3 +15,22 @@ from annoying.decorators import render_to
 @render_to('importsheet/poc.html')
 def poc(request):
     return dict()
+
+
+
+import httplib2
+from oauth2client.client import OAuth2WebServerFlow
+
+# Run through the OAuth flow and retrieve credentials
+flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE)
+authorize_url = flow.step1_get_authorize_url()
+print 'Go to the following link in your browser: ' + authorize_url
+code = raw_input('Enter verification code: ').strip()
+credentials = flow.step2_exchange(code)
+
+# Create an httplib2.Http object and authorize it with our credentials
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+drive_service = build('drive', 'v2', http=http)
+drive_service.files().copy(fileId=origin_file_id, body=copied_file).execute()
