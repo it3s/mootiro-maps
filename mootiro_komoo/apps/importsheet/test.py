22eb8d383073dd4b52e1c0dfffdd17fbb52f4c1c
André Casimiro <ah.casimiro@gmail.com>
2012-11-01 18:16:09 -0200

    Dict-objects for each row! :D

--- mootiro_komoo/apps/importsheet/test.py
+++ mootiro_komoo/apps/importsheet/test.py
@@ -1,12 +1,33 @@
-from gdata.spreadsheets.client import SpreadsheetsClient
-from gdata.gauth import AuthSubToken
+# -*- coding: utf-8 -*-
+'''
+This is script is an interactive way of exploring google spreadsheet API XMLs.
+  To use it, do:
+
+    fab shell
+    run apps/importsheet/test
+
+  ... inspect your variables. Good Luck! ...
+'''
 import feedparser
 
-access_token = 'ya29.AHES6ZQdeMTALUpatmy5yDdTH5okGwZANB3lVF1HULzhaFjS'
+from gdata.spreadsheets.client import SpreadsheetsClient
+from gdata.gauth import AuthSubToken
+
+from importsheet.token import get_access_token
+
+
+access_token = get_access_token()
 tok = AuthSubToken(token_string=access_token)
-key = '0Ahdnyvg2LXX-dDFITkdXd0hBNFBDczA4RFV2dVBVM0E'
+skey = '0Ahdnyvg2LXX-dDFITkdXd0hBNFBDczA4RFV2dVBVM0E'
+wkey = 'od6'
 cs = SpreadsheetsClient()
-data = cs.get_list_feed(key, 'od6', auth_token=tok)
+
+params = {'min-row': '1', 'max-row': '1'}
+data = cs.get_cells(skey, wkey, auth_token=tok, **params)
+h = feedparser.parse(str(data))
+
+params = {'min-row': '2'}
+data = cs.get_cells(skey, wkey, auth_token=tok, **params)
 d = feedparser.parse(str(data))
 
 # ... d is a browseable dict ... Good Luck!
--- mootiro_komoo/apps/importsheet/views.py
+++ mootiro_komoo/apps/importsheet/views.py
@@ -19,24 +19,49 @@ from .token import get_access_token, get_tok
 @render_to('importsheet/poc.html')
 def poc(request):
 
-    key = '0Ahdnyvg2LXX-dDFITkdXd0hBNFBDczA4RFV2dVBVM0E'
+    skey = '0Ahdnyvg2LXX-dDFITkdXd0hBNFBDczA4RFV2dVBVM0E'
+    wkey = 'od6'
     gd = google_drive_service(request)
     # gs = google_spreadsheets_service(request)
     gs = SpreadsheetsClient()
+    data = None
 
     # Copiar um documento
     if request.GET['action'] == 'copy':
-        data = gd.files().copy(fileId=key, body=dict(title="Copia")).execute()
+        data = gd.files().copy(fileId=skey, body=dict(title="Copia")).execute()
     
     # Baixar uma worksheet especifica
     if request.GET['action'] == 'worksheet':
-        data = gs.get_list_feed(key, 'od6', auth_token=get_tok())
+        data = gs.get_list_feed(skey, wkey, auth_token=get_tok())
         data = feedparser.parse(str(data))
         entries = []
         for entry in data['entries']:
             entries.append({k[4:]:v for k, v in entry.items() if k.startswith('ns4')})
         data = entries
 
+    # Baixar um intervalo de linhas específico
+    if request.GET['action'] == 'interval':
+        # é preciso usar um feed de células diretamente
+        params = {'min-row': '1', 'max-row': '1'}
+        data = gs.get_cells(skey, wkey, auth_token=get_tok(), **params)
+        h = feedparser.parse(str(data))
+        columns = {}
+        for e in h['entries']:
+            e = e['ns3_cell']
+            columns[e['col']] = e['inputvalue']
+
+        params = {'min-row': '2'}
+        data = gs.get_cells(skey, wkey, auth_token=get_tok(), **params)
+        d = feedparser.parse(str(data))
+        rows = {}
+        for e in d['entries']:
+            e = e['ns3_cell']
+            if not e['row'] in rows:
+                rows[e['row']] = {}
+            obj = rows[e['row']]  # the properly obj dict
+            attr = columns[e['col']]
+            obj[attr] = e['inputvalue']
+        data = rows
 
     print get_access_token()
 
