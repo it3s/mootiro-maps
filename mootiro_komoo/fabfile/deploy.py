a54ea874c91cddcb1f3b97a1b2a9f227f4125d1d
André Casimiro <ah.casimiro@gmail.com>
2012-12-18 14:44:25 -0200

    Added get_remote_db to fabfile

--- mootiro_komoo/fabfile/deploy.py
+++ mootiro_komoo/fabfile/deploy.py
@@ -1,28 +1,35 @@
 # -*- coding:utf-8 -*-
 from contextlib import contextmanager as _contextmanager
 from fabric.api import *
-from fabric.colors import red
-from fabric.contrib.console import confirm
+from fabric.contrib.files import exists
 
 
 __all__ = ('production', 'staging', 'deploy')
 
 def production():
     '''Setup env dict for running remote commands in production.'''
+    env.name = 'production'
     env.hosts = ['maps.mootiro.org']
+    env.django_settings = '--settings=settings.production'
     env.activate = 'source /home/login/.virtualenvs/mootiro_maps_env/bin/activate'
     env.project_folder = '/home/login/mootiro_maps/mootiro-maps/mootiro_komoo'
 
 
 def staging():
     '''Setup env dict for running remote commands in staging.'''
+    env.name = 'staging'
     env.hosts = ['maps.mootiro.org']
+    env.django_settings = '--settings=settings.staging'
     env.activate = 'source /home/login/.virtualenvs/mootiro_maps_staging_env/bin/activate'
     env.project_folder = '/home/login/mootiro_maps_staging/mootiro-maps/mootiro_komoo'
 
 
 @_contextmanager
 def virtualenv():
+    if not 'activate' in env or not 'project_folder' in env:
+        abort('Missing remote destination.\n'
+              'Usage: fab (production|staging) <command>.')
+
     with cd(env.project_folder):
         with prefix(env.activate):
             yield
@@ -30,40 +37,26 @@ def virtualenv():
 
 def deploy():
     '''Deploy application to staging or production.'''
-    if not 'activate' in env or not 'project_folder' in env:
-        abort('Missing deploy destination.\n'
-              'Usage: fab (production|stage) deploy.')
 
+    get_database_dump()
+
+
+def get_database_dump():
+    '''Dump remote database and transfers a copy of it locally.'''
+    import datetime
     with virtualenv():
-        run('pip freeze')
+        remote_path = '{}/backups/backupdb_{}_{}.json'.format(
+            env.project_folder,
+            env.name,
+            datetime.datetime.now().strftime('%Y_%m_%d')
+        )
+        if not exists(remote_path):
+            run('python manage.py dumpdata {} > {}'.format(
+                    env.django_settings, remote_path))
+        get(remote_path, './backups')
 
 
 
-
-# def deploy_to(env_='local'):
-#     if env_ == 'local':
-#         pass
-#     elif env_ == 'stage':
-#         pass
-#     elif env_ == 'production':
-#         config = {
-#             'project_folder': '~/mootiro_maps/mootiro-maps/mootiro_komoo',
-#             'virtualenv_name': 'mootiro_maps_env',
-#         }
-#     else:
-#         abort(red('Unknown environment {} to deploy.'.format(env_)))
-
-#     # with cd(config['project_folder']), \
-#     #      # prefix('source ~/.bashrc'), \
-#     #      prefix('workon {}'.format(config['virtualenv_name'])):
-#     wk = 'workon {}'.format(config['virtualenv_name'])
-#     print wk
-#     with prefix(wk):
-#         run('ls')
-
-
-# def deploy():
-#     deploy_to('production')
 
 
 ### Simulação Local ###
