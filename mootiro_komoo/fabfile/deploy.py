ca74a90859efbc40e7c8e5268d7ba49d2a9650d5
André Casimiro <ah.casimiro@gmail.com>
2012-12-19 13:42:44 -0200

    Added _migrate_database_dump

--- mootiro_komoo/fabfile/deploy.py
+++ mootiro_komoo/fabfile/deploy.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
+import os
+
 from contextlib import contextmanager as _contextmanager
 from fabric.api import *
+from fabric.colors import cyan, red, yellow, green
 from fabric.contrib.files import exists
+from fabric.contrib.console import confirm
+
+from .old_fabfile import sync_all, run as runapp
 
 
-__all__ = ('production', 'staging', 'deploy')
+# __all__ = ('production', 'staging', 'deploy')
 
 def production():
     '''Setup env dict for running remote commands in production.'''
@@ -35,27 +41,50 @@ def virtualenv():
             yield
 
 
+DBFILE = 'backupdb.json'
+
 def deploy():
     '''Deploy application to staging or production.'''
-
+    print(cyan('Local deploy simulation\n'))
+    print('Getting remote database copy...'))
     get_database_dump()
+
+    pyfile = prompt("Python script to do migration (empty if none):")
+    if pyfile:
+        _migrate_database_dump(pyfile, DBFILE)
+
+    sync_all(DBFILE)
+    runapp()
+    print(cyan('Deploying to remote'))
 
 
 def get_database_dump():
     '''Dump remote database and transfers a copy of it locally.'''
     import datetime
     with virtualenv():
-        remote_path = '{}/backups/backupdb_{}_{}.json'.format(
-            env.project_folder,
+        filename = 'backupdb_{}_{}.json'.format(
             env.name,
             datetime.datetime.now().strftime('%Y_%m_%d')
         )
+        local_path = DBFILE
+        remote_path = '{}/backups/{}'.format(env.project_folder, filename)
+        if os.path.exists(local_path):
+            if not confirm('Backup exists at {}. Do you want to overwrite it?'\
+                    .format(local_path)):
+                return
         if not exists(remote_path):
             run('python manage.py dumpdata {} > {}'.format(
                     env.django_settings, remote_path))
-        get(remote_path, './backups')
+        get(remote_path, local_path)
 
 
+def _migrate_database_dump(script, json_file=DBFILE):
+    '''Runs migration script in backupdb.json'''
+    if not os.path.exists(json_file):
+        abort('DB dump file {} does not exists. Aborting.'.format(json_file))
+
+    local('python {script} {inp} {out}'.format(script=script,
+            inp=json_file, out='temp.json'))
 
 
 
