dc8cb6606ab758b16da03d7119160e2f65f42a74
André Casimiro <ah.casimiro@gmail.com>
2013-01-21 19:01:36 -0200

    adjusts for separate supervisord instances
    now supervisord.conf was split and two different supervisors will
    be created for production and staging.
    
    Conflicts:
    
    	mootiro_komoo/supervisor/common.conf

--- mootiro_komoo/fabfile/remote/service.py
+++ mootiro_komoo/fabfile/remote/service.py
@@ -7,26 +7,32 @@ from fabric.colors import yellow
 __all__ = ('up', 'down', 'restart')
 
 
+def setup_supervisor():
+    with quiet():
+        # make sure supervisord is running
+        run('supervisord -c supervisor/{}.conf'.format(env.komoo_env))
+
+
 def up():
     '''Start remote application server.'''
-    # TODO: ensure supervisord is running!
+    setup_supervisor()
     with remote_virtualenv():
-        run('supervisorctl -c supervisor/supervisord.conf start {}' \
-            .format(env.komoo_env))
-    print yellow('Success, but it may take 1 minute to the server to go up.')
+        run('supervisorctl -c supervisor/{env}.conf start {env}' \
+            .format(env=env.komoo_env))
+    print yellow('Success, but it may take 1 minute for the server to go up.')
 
 
 def down():
     '''Stop remote application server.'''
-    # TODO: ensure supervisord is running!
+    setup_supervisor()
     with remote_virtualenv():
-        run('supervisorctl -c supervisor/supervisord.conf stop {}' \
-            .format(env.komoo_env))
+        run('supervisorctl -c supervisor/{env}.conf stop {env}' \
+            .format(env=env.komoo_env))
 
 
 def restart():
     '''Restart remote application server.'''
-    # TODO: ensure supervisord is running!
+    setup_supervisor()
     with remote_virtualenv():
-        run('supervisorctl -c supervisor/supervisord.conf restart {}' \
-            .format(env.komoo_env))
+        run('supervisorctl -c supervisor/{env}.conf restart {env}' \
+            .format(env=env.komoo_env))
