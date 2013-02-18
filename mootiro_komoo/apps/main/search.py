71ad6c1ebeed21e39d9157af7a11df1ef7683ab0
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-02-18 14:32:24 -0300

    index settings and type mapping working

--- mootiro_komoo/apps/main/search.py
+++ mootiro_komoo/apps/main/search.py
@@ -1,101 +1,109 @@
 # -*- coding=utf-8 -*-
+import requests
+import simplejson as json
 from django.conf import settings
-from pyes import ES
-# from pyes.queryset import generate_model
 
+ES = settings.ELASTICSEARCH_URL
 ES_INDEX = settings.ELASTICSEARCH_INDEX_NAME
 ES_TYPE = 'komoo_objects'
 
-MAPPING = {
-    "properties": {
-        "object_id": {"type": "string", "analyzer": "simple"},
-        "table_ref": {"type": "string", "analyzer": "simple"},
-        "name": {
-            "fields": {
-                "name": {
-                    "type": "string",
-                    "analyzer": "full_name"
+MAPPINGS_DICT = {
+    ES_TYPE: {
+        "properties": {
+            "object_id": {"type": "string", "analyzer": "simple"},
+            "table_ref": {"type": "string", "analyzer": "simple"},
+            "name": {
+                "fields": {
+                    "name": {
+                        "type": "string",
+                        "analyzer": "full_name"
+                    },
+                    "partial": {
+                        "type": "string",
+                        "search_analyzer": "full_name",
+                        "index_analyzer": "partial_name"
+                    }
                 },
-                "partial": {
-                    "type": "string",
-                    "search_analyzer": "full_name",
-                    "index_analyzer": "partial_name"
-                }
+                "type": "multi_field"
             },
-            "type": "multi_field"
-        },
-        # "description": {
-        #     "fields": {
-        #         "name": {
-        #             "type": "string",
-        #             "analyzer": "full_name"
-        #         },
-        #         "partial": {
-        #             "search_analyzer": "full_name",
-        #             "index_analyzer": "partial_name",
-        #             "type": "string"
-        #          }
-        #     },
-        #     "type": "multi_field"
-        # }
-    },
-    "settings": {
-        "analysis": {
-            "filter": {
-                "name_ngrams": {
-                    "side": "front",
-                    "max_gram": 10,
-                    "min_gram": 2,
-                    "type": "edgeNGram"
-                 }
-            },
-            "analyzer": {
-                "full_name": {
-                    "filter": [
-                        "standard",
-                        "lowercase",
-                        "asciifolding"
-                    ],
-                    "type": "custom",
-                    "tokenizer": "standard"
+            "description": {
+                "fields": {
+                    "name": {
+                        "type": "string",
+                        "analyzer": "full_name"
+                    },
+                    "partial": {
+                        "search_analyzer": "full_name",
+                        "index_analyzer": "partial_name",
+                        "type": "string"
+                     }
                 },
-                "partial_name": {
-                    "filter": [
-                        "standard",
-                        "lowercase",
-                        "asciifolding",
-                        "name_ngrams"
-                     ],
-                "type": "custom",
-                "tokenizer": "standard"
-              }
-           }
+                "type": "multi_field"
+            }
         }
     }
 }
-conn = ES(settings.ELASTICSEARCH_URL)  # Use HTTP
 
 
-def create_index():
-    conn.indices.create_index(ES_INDEX)
+SETTINGS_DICT = {
+    "settings": {
+        "index": {
+            "analysis": {
+                "filter": {
+                    "name_ngrams": {
+                        "side": "front",
+                        "max_gram": 10,
+                        "min_gram": 2,
+                        "type": "edgeNGram"
+                     }
+                },
+                "analyzer": {
+                    "full_name": {
+                        "filter": [
+                            "standard",
+                            "lowercase",
+                            "asciifolding"
+                        ],
+                        "type": "custom",
+                        "tokenizer": "standard"
+                    },
+                    "partial_name": {
+                        "filter": [
+                            "standard",
+                            "lowercase",
+                            "asciifolding",
+                            "name_ngrams"
+                         ],
+                    "type": "custom",
+                    "tokenizer": "standard"
+                  }
+               }
+            }
+        }
+    }
+}
 
 
-def delete_index():
-    conn.indices.delete_index(ES_INDEX)
+def es_url(url_spec):
+    return url_spec.format(ES=ES, INDEX=ES_INDEX, TYPE=ES_TYPE)
+
+
+def reset_index():
+    requests.delete(es_url('{ES}/{INDEX}'))
+    requests.put(es_url('{ES}/{INDEX}/'),
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(SETTINGS_DICT))
 
 
 def create_mapping():
-    conn.indices.put_mapping(ES_TYPE, MAPPING, [ES_INDEX])
+    requests.put(es_url('{ES}/{INDEX}/{TYPE}/_mapping'),
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(MAPPINGS_DICT))
 
 
-# def get_model():
-#     return generate_model(ES_INDEX, ES_TYPE)
-
-
-def refreseh_index(obj):
-    conn.indices.refresh(ES_INDEX)
+def refreseh_index():
+    requests.post(es_url('{ES}/{INDEX}]_refresh'))
 
 
 def index_object(obj):
-    conn.index(obj, ES_INDEX, ES_TYPE)
-    refreseh_index()
+    pass
