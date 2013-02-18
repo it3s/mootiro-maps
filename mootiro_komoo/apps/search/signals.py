b7211051d87857ee27a17c2703b085ac542bf248
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-02-18 17:33:13 -0300

    desabling temporarily search signal receiver

--- mootiro_komoo/apps/search/signals.py
+++ mootiro_komoo/apps/search/signals.py
@@ -10,6 +10,7 @@ index_object_for_search = django.dispatch.Signal(providing_args=["obj", ])
 @receiver(index_object_for_search)
 def index_object_callback(sender, obj, *a, **kw):
     if getattr(obj, 'id', None):
-        print '>>>Indexing', obj
-        index_object(obj)
+        # print '>>>Indexing', obj
+        # index_object(obj)
+        pass
 
