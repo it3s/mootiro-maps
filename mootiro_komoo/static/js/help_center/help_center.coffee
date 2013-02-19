6079d6bd885bf8662ddd5048b58ae2a52bf42db5
André Casimiro <ah.casimiro@gmail.com>
2013-02-19 19:52:15 -0300

    basic helpcenter modal added

--- mootiro_komoo/static/js/help_center/help_center.coffee
+++ mootiro_komoo/static/js/help_center/help_center.coffee
@@ -2,11 +2,53 @@ $ = jQuery
 
 class HelpCenter
     constructor: (btn_selector, @content_ids) ->
-        alert 'constructor'
         @button = $(btn_selector)
         @button.on 'click', @show
+        @modal_setup()
+
+    modal_setup: =>
+        html = "
+            <div id='help_center' class='modal hide fade'>
+                <div class='modal-header'>
+                    <button type='button' class='close' data-dismiss='modal'>×</button>
+                    <h3>Modal header</h3>
+                </div>
+                <div class='modal-body'>
+                    <p>One fine body…</p>
+                </div>
+            </div>
+        "
+        @$modal = $(html).modal {show: false}
+        $('body').append @$modal
 
     show: () =>
         console.log @content_ids
+        console.log @contents
+        @$modal.modal('show')
+
+    # TODO:use requires and put this json into other file
+    contents:
+        "organization:what_is":
+            "type": "question"
+            "data":
+                "question": "What is an organization?"
+                "answer": "An organization is ... (markdown string)"
+
+        "organization:page_tour":
+            "type": "tour"
+            "data":
+                "slides": [
+                    {
+                    "title": "Description"
+                    "body": "This is the organization description"
+                    "target": "#organization-description"
+                    },
+                    {
+                    "title": "Contact information"
+                    "body": "Here you'll find contact"
+                    "target": "#organization-contact"
+                    }
+                ]
+
 
 window.HelpCenter = HelpCenter
--- mootiro_komoo/static/js/help_center/help_center.js
+++ mootiro_komoo/static/js/help_center/help_center.js
@@ -9,13 +9,51 @@
     function HelpCenter(btn_selector, content_ids) {
       this.content_ids = content_ids;
       this.show = __bind(this.show, this);
-      alert('constructor');
+      this.modal_setup = __bind(this.modal_setup, this);
       this.button = $(btn_selector);
       this.button.on('click', this.show);
+      this.modal_setup();
     }
 
+    HelpCenter.prototype.modal_setup = function() {
+      var html;
+      html = "            <div id='help_center' class='modal hide fade'>                <div class='modal-header'>                    <button type='button' class='close' data-dismiss='modal'>×</button>                    <h3>Modal header</h3>                </div>                <div class='modal-body'>                    <p>One fine body…</p>                </div>            </div>        ";
+      this.$modal = $(html).modal({
+        show: false
+      });
+      return $('body').append(this.$modal);
+    };
+
     HelpCenter.prototype.show = function() {
-      return console.log(this.content_ids);
+      console.log(this.content_ids);
+      console.log(this.contents);
+      return this.$modal.modal('show');
+    };
+
+    HelpCenter.prototype.contents = {
+      "organization:what_is": {
+        "type": "question",
+        "data": {
+          "question": "What is an organization?",
+          "answer": "An organization is ... (markdown string)"
+        }
+      },
+      "organization:page_tour": {
+        "type": "tour",
+        "data": {
+          "slides": [
+            {
+              "title": "Description",
+              "body": "This is the organization description",
+              "target": "#organization-description"
+            }, {
+              "title": "Contact information",
+              "body": "Here you'll find contact",
+              "target": "#organization-contact"
+            }
+          ]
+        }
+      }
     };
 
     return HelpCenter;
