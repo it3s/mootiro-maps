f10fa3cd3c2c86d09ac28ae9e8acf2b03e7f07ca
André Casimiro <ah.casimiro@gmail.com>
2013-02-19 21:23:55 -0300

    implemented js templating for help contents

--- mootiro_komoo/static/js/help_center/help_center.coffee
+++ mootiro_komoo/static/js/help_center/help_center.coffee
@@ -1,54 +1,111 @@
 $ = jQuery
 
+
 class HelpCenter
-    constructor: (btn_selector, @content_ids) ->
+    constructor: (btn_selector, content_ids) ->
         @button = $(btn_selector)
         @button.on 'click', @show
+        @contents = (@contents_config[cid] for cid in content_ids)
         @modal_setup()
 
-    modal_setup: =>
-        html = "
-            <div id='help_center' class='modal hide fade'>
-                <div class='modal-header'>
-                    <button type='button' class='close' data-dismiss='modal'>×</button>
-                    <h3>Modal header</h3>
-                </div>
-                <div class='modal-body'>
-                    <p>One fine body…</p>
-                </div>
-            </div>
+    question_tpl:
         "
-        @$modal = $(html).modal {show: false}
+        <article>
+            <h3><%= question %></h3>
+            <p><%= answer %></p>
+        </article>
+        "
+
+    tutorial_tpl:
+        "
+        tourtorial
+        "
+
+    # TODO: put this into an html file
+    modal_tpl:
+        "
+        <div id='help_center' class='modal hide fade'>
+            <div class='modal-header'>
+                <button type='button' class='close' data-dismiss='modal'>×</button>
+                <h2>Modal header</h2>
+            </div>
+            <section class='modal-body'>
+                <ul>
+                    <% for (var i = 0; i < contents.length; i++) { %>
+                        <li>
+                            <!--!--------- QUESTION ----------->
+                            <% if (contents[i].type == 'question') { %>
+                            <article>
+                                <h3><%= contents[i].title %></h3>
+                                <p><%= contents[i].body %></p>
+                            </article>
+                            <% } %>
+                            
+                            <!--!--------- TUTORIAL ----------->
+                            <% if (contents[i].type == 'tutorial') { %>
+                            <article>
+                                <h3><%= contents[i].title %></h3>
+                                <p><%= contents[i].body %></p>
+                            </article>
+                            <div id='tutorial' class='tutorial'>
+                                <% for (var j = 0; j < contents[i].slides.length; j++) { %>
+                                <div
+                                  data-target='<%= contents[i].slides[j].target %>'
+                                  data-location='<%= contents[i].slides[j].location %>'
+                                  data-arrow='<%= contents[i].slides[j].arrow %>'
+                                >
+                                    <h3><%= contents[i].slides[j].title %></h3>
+                                    <p><%= contents[i].slides[j].body %></p>
+                                </div>
+                                <% } %>
+                            </div>
+                            <!--!------------------------------>
+
+                            <% } %>
+                        </li>
+                    <% } %>
+                </ul>
+            </section>
+        </div>
+        "
+
+    modal_setup: =>
+        html = _.template @modal_tpl, {contents: @contents}
+        console.log "@contents", @contents
+        @$modal = $(html).modal {show: true}
         $('body').append @$modal
 
     show: () =>
-        console.log @content_ids
-        console.log @contents
         @$modal.modal('show')
 
     # TODO:use requires and put this json into other file
-    contents:
+    contents_config:
+
         "organization:what_is":
             "type": "question"
-            "data":
-                "question": "What is an organization?"
-                "answer": "An organization is ... (markdown string)"
+            "title": "What is an organization?"
+            "body": "An organization is ... (markdown string)"
 
         "organization:page_tour":
-            "type": "tour"
-            "data":
-                "slides": [
-                    {
-                    "title": "Description"
-                    "body": "This is the organization description"
-                    "target": "#organization-description"
-                    },
-                    {
-                    "title": "Contact information"
-                    "body": "Here you'll find contact"
-                    "target": "#organization-contact"
-                    }
-                ]
+            "type": "tutorial"
+            "title": "Organization page tour"
+            "body": "Take the tour of this page"
+            "slides": [
+                {
+                "title": "Description"
+                "body": "This is the organization description"
+                "target": "#organization-description"
+                "location": "tl"
+                "arrow": "tc"
+                },
+                {
+                "title": "Contact information"
+                "body": "Here you'll find contact"
+                "target": "#organization-contact"
+                "location": "tl"
+                "arrow": "tc"
+                }
+            ]
 
 
 window.HelpCenter = HelpCenter
--- mootiro_komoo/static/js/help_center/help_center.js
+++ mootiro_komoo/static/js/help_center/help_center.js
@@ -7,52 +7,70 @@
   HelpCenter = (function() {
 
     function HelpCenter(btn_selector, content_ids) {
-      this.content_ids = content_ids;
       this.show = __bind(this.show, this);
       this.modal_setup = __bind(this.modal_setup, this);
+      var cid;
       this.button = $(btn_selector);
       this.button.on('click', this.show);
+      this.contents = (function() {
+        var _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = content_ids.length; _i < _len; _i++) {
+          cid = content_ids[_i];
+          _results.push(this.contents_config[cid]);
+        }
+        return _results;
+      }).call(this);
       this.modal_setup();
     }
 
+    HelpCenter.prototype.question_tpl = "        <article>            <h3><%= question %></h3>            <p><%= answer %></p>        </article>        ";
+
+    HelpCenter.prototype.tutorial_tpl = "        tourtorial        ";
+
+    HelpCenter.prototype.modal_tpl = "        <div id='help_center' class='modal hide fade'>            <div class='modal-header'>                <button type='button' class='close' data-dismiss='modal'>×</button>                <h2>Modal header</h2>            </div>            <section class='modal-body'>                <ul>                    <% for (var i = 0; i < contents.length; i++) { %>                        <li>                            <!--!--------- QUESTION ----------->                            <% if (contents[i].type == 'question') { %>                            <article>                                <h3><%= contents[i].title %></h3>                                <p><%= contents[i].body %></p>                            </article>                            <% } %>                                                        <!--!--------- TUTORIAL ----------->                            <% if (contents[i].type == 'tutorial') { %>                            <article>                                <h3><%= contents[i].title %></h3>                                <p><%= contents[i].body %></p>                            </article>                            <div id='tutorial' class='tutorial'>                                <% for (var j = 0; j < contents[i].slides.length; j++) { %>                                <div                                  data-target='<%= contents[i].slides[j].target %>'                                  data-location='<%= contents[i].slides[j].location %>'                                  data-arrow='<%= contents[i].slides[j].arrow %>'                                >                                    <h3><%= contents[i].slides[j].title %></h3>                                    <p><%= contents[i].slides[j].body %></p>                                </div>                                <% } %>                            </div>                            <!--!------------------------------>                            <% } %>                        </li>                    <% } %>                </ul>            </section>        </div>        ";
+
     HelpCenter.prototype.modal_setup = function() {
       var html;
-      html = "            <div id='help_center' class='modal hide fade'>                <div class='modal-header'>                    <button type='button' class='close' data-dismiss='modal'>×</button>                    <h3>Modal header</h3>                </div>                <div class='modal-body'>                    <p>One fine body…</p>                </div>            </div>        ";
+      html = _.template(this.modal_tpl, {
+        contents: this.contents
+      });
+      console.log("@contents", this.contents);
       this.$modal = $(html).modal({
-        show: false
+        show: true
       });
       return $('body').append(this.$modal);
     };
 
     HelpCenter.prototype.show = function() {
-      console.log(this.content_ids);
-      console.log(this.contents);
       return this.$modal.modal('show');
     };
 
-    HelpCenter.prototype.contents = {
+    HelpCenter.prototype.contents_config = {
       "organization:what_is": {
         "type": "question",
-        "data": {
-          "question": "What is an organization?",
-          "answer": "An organization is ... (markdown string)"
-        }
+        "title": "What is an organization?",
+        "body": "An organization is ... (markdown string)"
       },
       "organization:page_tour": {
-        "type": "tour",
-        "data": {
-          "slides": [
-            {
-              "title": "Description",
-              "body": "This is the organization description",
-              "target": "#organization-description"
-            }, {
-              "title": "Contact information",
-              "body": "Here you'll find contact",
-              "target": "#organization-contact"
-            }
-          ]
-        }
+        "type": "tutorial",
+        "title": "Organization page tour",
+        "body": "Take the tour of this page",
+        "slides": [
+          {
+            "title": "Description",
+            "body": "This is the organization description",
+            "target": "#organization-description",
+            "location": "tl",
+            "arrow": "tc"
+          }, {
+            "title": "Contact information",
+            "body": "Here you'll find contact",
+            "target": "#organization-contact",
+            "location": "tl",
+            "arrow": "tc"
+          }
+        ]
       }
     };
 
