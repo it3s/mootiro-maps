a95ce867d36dcccec50bdbb8d3361434436c0503
André Casimiro <ah.casimiro@gmail.com>
2013-02-28 16:48:16 -0300

    Finished tours!

--- mootiro_komoo/static/js/help_center/help_center.js
+++ mootiro_komoo/static/js/help_center/help_center.js
@@ -282,6 +282,188 @@
             "selector": "div.button-new"
           }
         ]
+      },
+      "community_show": {
+        "slides": [
+          {
+            "title": "Título da comunidade",
+            "body": "Este é o nome com o qual o território foi cadastrado.",
+            "selector": ".main-column h2.title"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Perfil da comunidade",
+            "body": "O preenchimento deste campo mostra uma descrição da comunidade, como a localização, histórico, perfil socioeconômico da população etc.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "No mapa",
+            "body": "O mapa traz uma prévia visualização da comunidade.",
+            "selector": "#map-preview",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Comunidades próximas",
+            "body": "O campo mostra outras comunidades cadastradas no MootiroMaps que estão próximas a esta.",
+            "selector": ".right-bar .nearby-communities",
+            "options": "tipLocation:left;"
+          }
+        ]
+      },
+      "organization_show": {
+        "slides": [
+          {
+            "title": "Título da organização",
+            "body": "Este é o nome com o qual a organização foi cadastrada.",
+            "selector": ".organization-header"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página, o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Descrição da organização",
+            "body": "O preenchimento deste campo mostra a descrição da organização, como temática de atuação, objetivos, projetos e serviços, parceiros, financiadores etc.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "No mapa",
+            "body": "O mapa traz uma prévia visualização da organização no território.",
+            "selector": "#map-preview",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Projetos",
+            "body": "O campo apresenta a quais projetos de mapeamento esta organização está relacionada.",
+            "selector": ".projects-tag-header",
+            "options": "tipLocation:left;"
+          }
+        ]
+      },
+      "need_show": {
+        "slides": [
+          {
+            "title": "Título da necessidade",
+            "body": "Este é o nome com o qual a necessidade foi cadastrada.",
+            "selector": ".need-title"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página, o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Descrição da necessidade",
+            "body": "O preenchimento deste campo mostra a descrição da necessidade e permite que os usuários consigam compreender a natureza da problemática.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:right;"
+          }, {
+            "title": "No mapa",
+            "body": "O mapa traz uma prévia visualização da necessidade no território.",
+            "selector": "#map-preview",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Proposta de resolução",
+            "body": "Neste campo é possível inserir novas propostas de resolução da necessidade.",
+            "selector": ".need-proposals",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Necessidades semelhantes",
+            "body": "O MootiroMaps relaciona necessidades semelhantes ou próximas para facilitar que os usuários troquem experiências e discutam como resolvê-las.",
+            "selector": "#related-needs",
+            "options": "tipLocation:left;"
+          }
+        ]
+      },
+      "resource_show": {
+        "slides": [
+          {
+            "title": "Título do recurso",
+            "body": "Este é o nome com o qual o recurso foi cadastrado.",
+            "selector": "h2.title"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página, o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Descrição do recurso",
+            "body": "O preenchimento deste campo mostra a descrição do recurso, como localização, horários de atendimento, temática, projetos e serviços etc.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:right;"
+          }, {
+            "title": "No mapa",
+            "body": "O mapa traz uma prévia visualização do recurso no território.",
+            "selector": "#map-preview",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Recursos semelhantes",
+            "body": "O campo permite que os usuários identifiquem recursos semelhantes ou próximos.",
+            "selector": ".related-resource",
+            "options": "tipLocation:left;",
+            "offsetX": -10,
+            "offsetY": -25
+          }
+        ]
+      },
+      "investment_show": {
+        "slides": [
+          {
+            "title": "Título do investimento",
+            "body": "Este é o nome com o qual o investimento foi cadastrado.",
+            "selector": "h2.title"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página, o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Descrição do investimento",
+            "body": "O preenchimento deste campo mostra a descrição do investimento, como o objetivo, localização, temática, público-alvo etc.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:right;"
+          }
+        ]
+      },
+      "project_show": {
+        "slides": [
+          {
+            "title": "Título do projeto",
+            "body": "Este é o nome com o qual o projeto foi cadastrado.",
+            "selector": "h2.title"
+          }, {
+            "title": "Edição do conteúdo",
+            "body": "Para editar o conteúdo dessa página, o usuário deve clicar neste botão.",
+            "selector": "div.view-edit-btn",
+            "options": "tipLocation:bottom;nubPosition:top-right;",
+            "offsetX": -135
+          }, {
+            "title": "Descrição do projeto",
+            "body": "O preenchimento deste campo mostra a descrição do projeto, como o objetivo, localização, temática, público-alvo, apoiadores etc.",
+            "selector": ".main-column .mark-down",
+            "options": "tipLocation:right;"
+          }, {
+            "title": "Objetos relacionados",
+            "body": "A listagem mostra objetos (organizações, recursos, necessidades etc) que foram cadastrados e relacionados a esse projeto.",
+            "selector": ".view-info-buttons",
+            "options": "tipLocation:top;",
+            "offsetX": 50
+          }, {
+            "title": "No mapa",
+            "body": "O mapa traz uma prévia visualização do recurso no território.",
+            "selector": "#map-preview",
+            "options": "tipLocation:left;"
+          }, {
+            "title": "Participantes",
+            "body": "O campo mostra o usuário administrador e os colaboradores.",
+            "selector": ".view-info-btn:last"
+          }
+        ]
       }
     };
 
