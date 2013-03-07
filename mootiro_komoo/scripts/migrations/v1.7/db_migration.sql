a016dee480828a838610a3d7ab9a0577a800823d
Luiz <luiz.armesto@gmail.com>
2013-03-07 15:22:37 -0300

    renaming migration script

--- mootiro_komoo/scripts/migrations/v1.7/db_migration.sql
+++ mootiro_komoo/scripts/migrations/v1.7/db_migration.sql
@@ -1,3 +1,4 @@
 \i scripts/migrations/v1.7/remove_branches.sql
 \i scripts/migrations/v1.7/generic_relations_georefobject.sql
 \i scripts/migrations/v1.7/adding_new_tags.sql
+\i scripts/migrations/v1.7/change_project.sql
