6f3162ac9e0c4b6495a5bcb26521956610bc837a
Luiz <luiz.armesto@gmail.com>
2013-03-12 13:52:20 -0300

    verify if the remote user info was configured

--- mootiro_komoo/fabfile/remote/__init__.py
+++ mootiro_komoo/fabfile/remote/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
 
 from fabric.api import env
-from .secret import *
+
+try:
+    from .secret import *
+except ImportError:
+    print 'Warning: You should copy "fabfile/remote/secret.py-dist" to "fabfile/remote/secret.py" and then fill with your remote "user" and "password".'
+
 
 from .base import staging, production
 from .service import *
