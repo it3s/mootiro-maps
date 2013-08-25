a77d05332cf16b61c1a63753dcf8767d575dd545
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-08-25 15:05:20 -0300

    improve dependencies handling to use local instalations instead global

--- mootiro_komoo/scripts/bootstrap.sh
+++ mootiro_komoo/scripts/bootstrap.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/env sh
 
-sudo apt-get install nodejs npm rubygems python-dev -y
-sudo apt-get install libev4 libev-dev libevent-2.0-5 libevent-dev
+# TODO check for npm or install nvm
+
+# TODO check ruby / rubygems / bundler or install them
+
+# system dependencies
+# TODO check if its debian-based linux distro
+sudo apt-get python-dev install libev4 libev-dev libevent-2.0-5 libevent-dev -y
 
 # install and configure rabbitmq for working with celery
 sudo apt-get install rabbitmq-server -y
@@ -9,17 +14,19 @@ sudo rabbitmqctl add_user komoo komoo
 sudo rabbitmqctl add_vhost mootiro_maps_mq
 sudo rabbitmqctl set_permissions -p mootiro_maps_mq komoo ".*" ".*" ".*"
 
+# python dependencies
 pip install -r settings/requirements.txt
 
-sudo npm install -g coffee-script@1.2
-sudo npm install -g requirejs@2.1
+#nodejs dependencies
+npm install
 
-sudo gem install --version '~> 0.8.8' rb-inotify
-sudo gem install listen
-sudo gem install sass
+# ruby dependecies
+bundle install
 
 # apply patch for django 1.3
 fab build_environment
+
+mkdir ../logs/
 
 # run tests
 fab test
