b99e80ffb5a2767026df8fb3a5d2b6147dbdad19
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-08-25 15:26:23 -0300

    add node and ruby dependecy instuctions to bootstrap.sh

--- mootiro_komoo/scripts/bootstrap.sh
+++ mootiro_komoo/scripts/bootstrap.sh
@@ -1,11 +1,27 @@
 #! /usr/bin/env sh
 
-# TODO check for npm or install nvm
+command -v npm >/dev/null 2>&1 || {
+    echo >&2 "NPM is required. You can install it whith:"
+    echo >&2 "    curl https://raw.github.com/creationix/nvm/master/install.sh | sh ";
 
-# TODO check ruby / rubygems / bundler or install them
+    echo >&2 "Remember to add nvm to your rc file, and source it."
+    echo >&2 "    [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh "
+    echo >&2 "\nEnsure you have installed node and npm"
+    exit 1;
+}
+
+command -v bundle >/dev/null 2>&1 || {
+    echo >&2 "Bundler is required. You can install it whith:"
+    echo >&2 "    curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby";
+    echo >&2 "\nRemember to add rvm to your path and source it"
+    echo >&2 "\nEnter the commands bellow to automatize your gemset creation/activation:"
+    echo >&2 "    echo \"1.9.3\" > .ruby-version"
+    echo >&2 "    echo \"mootiro_maps\" > .ruby-gemset"
+    echo >&2 "    # cd again in this directory, let the magic happen."
+    exit 1;
+}
 
 # system dependencies
-# TODO check if its debian-based linux distro
 sudo apt-get python-dev install libev4 libev-dev libevent-2.0-5 libevent-dev -y
 
 # install and configure rabbitmq for working with celery
@@ -27,7 +43,4 @@ bundle install
 fab build_environment
 
 mkdir ../logs/
-
-# run tests
-fab test
 
