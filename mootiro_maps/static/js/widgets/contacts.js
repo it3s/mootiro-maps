bb2b7ac09af2af93a64e5f0780c99f500379651c
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-01-22 14:59:26 -0200

    render key/value pairs

--- mootiro_maps/static/js/widgets/contacts.js
+++ mootiro_maps/static/js/widgets/contacts.js
@@ -12,6 +12,7 @@ var ContactsWidget = {
 
   onLoad: function() {
     this.field = $("#id_contacts");
+    this.contactsListField = $(".contacts-list");
     var initialValues = JSON.parse(this.field.val());
 
     this.loadInitialValues(initialValues);
@@ -22,18 +23,38 @@ var ContactsWidget = {
     return this.field.data("key-names")[key];
   },
 
+  keyOrder: function() {
+    return [
+      'address',
+      'compl',
+      'city',
+      'postal_code',
+      'phone',
+      'facebook',
+      'email',
+      'twitter',
+      'site',
+      '---',
+    ];
+  },
+
   loadInitialValues: function(initialValues) {
     var module = this;  // reference for the ContactsWidget module itself
 
-    _.each(initialValues, function(value, key) {
-      if (typeof value !== "undefined" && value !== null) {
-        module.renderKeyValuePair(module.getKeyName(key), value);
-      }
+    _.each(module.keyOrder(), function(key) {
+      var value = initialValues[key];
+      module.renderKeyValuePair(key, module.getKeyName(key), value);
     });
   },
 
-  renderKeyValuePair: function(key, value) {
+  renderKeyValuePair: function(key, keyName, value) {
     console.log("rendering contacts: key=%s value=%s", key, value);
+    this.contactsListField.append("" +
+      "<div class=\"contact-kv\">" +
+        "<label>" + keyName + "</label>" +
+        "<input type='text' value='" + (value || "") + "' data-key='" + key + "' />" +
+      "</div>"
+    );
   },
 
 
