385f280f41629d8be843fdde0a133dbc92581ec8
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-04-01 11:31:56 -0300

    use same autocomplte structure from django widget

--- mootiro_maps/static/js/relations/edit.js
+++ mootiro_maps/static/js/relations/edit.js
@@ -1,9 +1,25 @@
 $(function(){
-  $("#target").autocomplete({
+
+  $("#id_target_autocomplete").autocomplete({
     source: [
       'object 1',
       'blablabal',
       'resource bla',
-    ]
+    ],
+    focus: function(event, ui) {
+      $("#id_target_autocomplete").val(ui.item.label);
+      return false;
+    },
+    select: function(event, ui) {
+      $("#id_target_autocomplete").val(ui.item.label);
+      $("#id_target").val(ui.item.value);
+      return false;
+    },
+    change: function(event, ui) {
+      if(!ui.item || !$("#id_target_autocomplete").val()){
+          $("#id_target_autocomplete").val('');
+          $("#id_target").val('');
+      }
+    }
   });
 });
