95071802014b54cc0eab89119f92a3d6e1ccf67d
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-04-03 11:08:12 -0300

    update relations json and improve relations widget

--- mootiro_maps/static/js/relations/edit.js
+++ mootiro_maps/static/js/relations/edit.js
@@ -1,4 +1,4 @@
-$(function(){
+(function() {
 
   var autocompleteWidget = function(container) {
     return {
@@ -7,44 +7,90 @@ $(function(){
       init: function() {
         this.input = this.container.find('.target_autocomplete');
         this.field = this.container.find('.target');
+        this.dropdown = this.container.find('select[name=relation_type]');
         this.startPlugin();
+      },
+
+      onFocus: function(_this, ui) {
+        _this.input.val(ui.item.label);
+        return false;
+      },
+
+      onSelect: function(_this, ui) {
+        _this.input.val(ui.item.label);
+        _this.field.val(ui.item.value);
+        _this.container.trigger('relations:update');
+        return false;
+      },
+
+      onChange: function(_this, ui) {
+        if(!ui.item || !_this.input.val()){
+          _this.field.val('');
+          _this.input.val('');
+          _this.container.trigger('relations:update');
+        }
       },
 
       startPlugin: function() {
         var _this = this;
         _this.input.autocomplete({
           source: _this.input.attr('data-autocomplete'),
-          focus: function(event, ui) {
-            _this.input.val(ui.item.label);
-            return false;
-          },
-          select: function(event, ui) {
-            _this.input.val(ui.item.label);
-            _this.field.val(ui.item.value);
-            return false;
-          },
-          change: function(event, ui) {
-            if(!ui.item || !_this.input.val()){
-                _this.field.val('');
-                _this.input.val('');
-            }
-          }
+          focus:  function(event, ui) { return _this.onFocus(_this, ui); },
+          select: function(event, ui) { return _this.onSelect(_this, ui); },
+          change: function(event, ui) { return _this.onChange(_this, ui); },
         });
+
+        _this.dropdown.change(function() { _this.container.trigger('relations:update'); });
       },
     };
-  }
+  };
 
-  var addNewRelation = function() {
-    var relationTpl = $('#relation-item-tpl').html();
-    var relationItem = $(relationTpl);
-    $('.relations-edit .relations-list').append(relationItem);
-    autocompleteWidget(relationItem).init();
-  }
+  var relationsManager = function() {
+    return {
+      init: function() {
+        this.relationsList = $('.relations-edit .relations-list');
+        this.relationTpl = $('#relation-item-tpl').html();
+        this.addButton = $('.add-relation');
+        this.input = $(".relations-edit input[name='relations_json']");
 
-  $('.add-relation').click(function(){
-    addNewRelation();
+        this.addNewRelation();  // starts with at least one relation entry
+        this.bindEvents();
+      },
+
+      addNewRelation: function() {
+        var _this = this;
+        var relationItem = $(_this.relationTpl);
+        this.relationsList.append(relationItem);
+        relationItem.on('relations:update', function() { _this.updateInput(_this); });
+        autocompleteWidget(relationItem).init();
+      },
+
+      updateInput: function(_this) {
+        var relations = [];
+        _this.relationsList.find('.relation-item').each(function(i, el) {
+          var $el = $(el);
+          var rel = $el.find('[name=relation_type]').val();
+          var target = $el.find('.target').val()
+          if (target && target.length > 0 && rel && rel.length > 0) {
+            relations.push({
+              direction: rel[0],
+              rel_type : rel.substring(1, rel.length),
+              target   : target,
+            })
+          }
+        });
+        _this.input.val(JSON.stringify(relations));
+      },
+
+      bindEvents: function() {
+        var _this = this;
+        _this.addButton.click(function(){ _this.addNewRelation() });
+      }
+    };
+  };
+
+  $(function(){
+    relationsManager().init();
   });
 
-  addNewRelation();
-
-});
+})();
