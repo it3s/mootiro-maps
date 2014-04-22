fa4ecbbaacbfc2b17ffb9d7df196829753d67aa4
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-04-22 16:09:33 -0300

    migrate communities to relations

--- scripts/migrations/20140314_add_relations/data_migration.py
+++ scripts/migrations/20140314_add_relations/data_migration.py
@@ -3,13 +3,23 @@
 from komoo_resource.models import Resource
 from organization.models import Organization
 from community.models import Community
+from need.models import Need
+from relations.models import Relation
 
 def migrate_community_relations():
-    for model in [Resource, Organization]:
+    for model in [Resource, Organization, Need]:
         for obj in model.objects.all():
-            # TODO implement-me
-            # obj -> community  <= -contains
-            pass
+            if obj.community.count() > 0:
+                obj_oid = Relation.build_oid(obj)
+                relations = [
+                    {
+                        'target': 'c{id}'.format(id=comm.id),
+                        'rel_type': 'contains',
+                        'direction': '-',
+                    } for comm in obj.community.all()
+                ]
+                if relations:
+                    Relation.edit(obj_oid, relations)
 
 def run():
     migrate_community_relations()
