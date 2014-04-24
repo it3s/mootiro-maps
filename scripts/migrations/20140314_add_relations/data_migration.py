37231010b6105611fcfe0879ace184407cb68811
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-04-24 15:42:51 -0300

    migrate investments

--- scripts/migrations/20140314_add_relations/data_migration.py
+++ scripts/migrations/20140314_add_relations/data_migration.py
@@ -4,6 +4,7 @@ from komoo_resource.models import Resource
 from organization.models import Organization
 from community.models import Community
 from need.models import Need
+from investment.models import Investment
 from relations.models import Relation
 
 def migrate_community_relations():
@@ -21,7 +22,25 @@ def migrate_community_relations():
                 if relations:
                     Relation.edit(obj_oid, relations)
 
+def migrate_investments():
+    for inv in Investment.objects.all():
+        if not inv.investor.is_anonymous and inv.investor.content_object:
+            oid = Relation.build_oid(inv.investor.content_object)
+            relation = [{
+                'target': Relation.build_oid(inv.grantee),
+                'rel_type': 'investment',
+                'direction': '+',
+                'metadata': {
+                    'start_date': inv.date.strftime('%d/%m/%Y') if inv.date else None,
+                    'end_date': inv.end_date.strftime('%d/%m/%Y') if inv.end_date else None,
+                    'description': inv.name,
+                    'value': inv.value,
+                }
+            }]
+            Relation.edit(oid, relation)
+
+
 def run():
     migrate_community_relations()
-
+    migrate_investments()
 
