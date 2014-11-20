94523c789d2f4fd5bc624f969d3508e56052fc73
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2014-11-20 00:04:22 -0200

    send data to redis

--- mootiro_maps/lib/migrate/tasks.py
+++ mootiro_maps/lib/migrate/tasks.py
@@ -1,4 +1,13 @@
 # coding: utf-8
+from redis import StrictRedis
+import simplejson as json
+
+redis = StrictRedis(host='10.0.2.2', port=6379, db=0)
+
+def send_to_redis(obj):
+    oid = obj['oid']
+    redis.rpush('mootiro:migrations', oid)
+    redis.set('mootiro:{}'.format(oid), json.dumps(obj))
 
 def migrate_all():
     migrate_organizations()
@@ -8,7 +17,7 @@ def migrate_organizations():
 
     for org in Organization.objects.all():
         obj = parse_organization(org)
-        # send to redis json.dups(obj)
+        send_to_redis(obj)
 
 def parse_organization(org):
     return {
@@ -16,15 +25,15 @@ def parse_organization(org):
         'mootiro_type': 'organization',
         'name': org.name,
         'description': org.description,
-        "created_at": org.creation_date,
-        "updated_at": org.last_update,
+        "created_at": str(org.creation_date),
+        "updated_at": str(org.last_update),
         "contacts": org.contacts,
         "aditional_info": {
-            'target_audiences':  [ta.name for ta in org.target_audiences.all()],
+            'target_audiences':  [ta.name for ta in org.target_audiences.all() if ta],
             'short_description': org.short_description,
-            'creator': org.creator.name,
+            'creator': org.creator.name if org.creator else None,
         },
-        'tags': [tag.name for tag in org.tags.all()] + [c.get_translated_name() for c in org.categories.all()],
+        'tags': [tag.name for tag in org.tags.all() if tag] + [c.get_translated_name() for c in org.categories.all() if c],
         'geometry': org.geometry.wkt,
         # logo ? logo_category ? logo_choice ?
     }
